dd4e312582f6fe1b5b8f9fe5815a2eade55fb35e
sbe dev <sbedev88@gmail.com>
2023-03-15 21:13:02 +0100

    Implement Four Of A kind score category and tests case

---
+++ src/main/java/com/gitlab/yatzygame/score/FourOfAkindScoreCategory.java
@@ -0,0 +1,37 @@
+package com.gitlab.yatzygame.score;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * The FourOfAkindScoreCategory class implements the IScoreCategory interface and represents the
+ * "FourOfAkindScore" category in the Yatzy game.
+ */
+
+public class FourOfAkindScoreCategory implements IScoreCategory{
+
+    /**
+     * This implementation of the FourOfAkind score(), which is the sum of the Four Of A kind of dice rolled.
+     *
+     * @param values the values of the dice in the roll
+     * @return the score value
+     */
+    @Override
+    public int score(int[] values) {
+
+        // Utilisation de Collectors.groupingBy pour obtenir un Map des éléments et de leur nombre d'occurrences
+        Map<Integer, Long> countEachElement = Arrays.stream(values).boxed()
+                .collect(Collectors.groupingBy(n -> n, Collectors.counting()));
+
+        // Utilisation de Map.entrySet() et Stream pour récupérer le key = 4 occurences
+        Optional<Integer> fourOfAkind = countEachElement.entrySet().stream()
+                .filter(entry -> entry.getValue() >= 4)
+                .map(Map.Entry::getKey).findFirst();
+
+           return fourOfAkind.map(v -> v * 4).orElse(0);
+
+    }
+}
+
---
+++ src/test/java/com/gitlab/yatzygame/score/FourOfAkindScoreCategoryTest.java
@@ -0,0 +1,34 @@
+package com.gitlab.yatzygame.score;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FourOfAkindScoreCategoryTest {
+    private final IScoreCategory strategy = new FourOfAkindScoreCategory();
+
+    @Test
+    public void testScoreWithoutFourOfAkind() {
+        int[] values = {1, 6, 2, 2, 2};
+        int expectedScore = 0;
+        int actualScore = strategy.score(values);
+        assertEquals(expectedScore, actualScore);
+    }
+
+    @Test
+    public void testScoreWithFourOfAkind() {
+        int[] values = {1, 5, 5, 5, 5};
+        int expectedScore = 20;
+        int actualScore = strategy.score(values);
+        assertEquals(expectedScore, actualScore);
+    }
+
+    @Test
+    public void testScoreWithAllSameKind() {
+        int[] values = {5, 5, 5, 5, 5};
+        int expectedScore = 20;
+        int actualScore = strategy.score(values);
+        assertEquals(expectedScore, actualScore);
+    }
+
+}
